a6753279631fc511981df7e385954ac5d96bc1fc
nightfalldlp <57466256+nightfalldlp@users.noreply.github.com>
2019-12-06 10:38:19 -0800

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Some pakages are needed,
+# Packages needed,
 # Tensorflow, keras, and flask
 # If you are unfamiliar with how to install these, please go to their official website
 
@@ -12,7 +12,7 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 
 
-# Assume packages are installed, and the first step would be to make a Flask object
+# Assume packages are installed, then first step would be to make a Flask object
 application = Flask(__name__)
 
 '''
@@ -20,9 +20,9 @@ Example 1: GET request
 To quickly test the flask application, we want to create a route for a simple GET request
 If we type/curl a url in a web browser/terminal, we want to see something return back.
 
-This would be the simplest test example
-application.route is a decorator that route any traffic to '/' to call the function running
-in this case, if we curl http://127.0.0.1:5000/, you could see "Testing the landing page! This worked" under
+This is a simple example to ensure the server is working
+@application.route is a decorator that routes any traffic to path '/' to call the function running
+in this case, if we curl http://127.0.0.1:5000/, you should get a response of "Testing the landing page! This worked" 
 '''
 @application.route('/', methods=['GET'])
 def running():
@@ -32,10 +32,10 @@ def running():
 '''
 Example 2: POST request
 A Post request will send parameters in the post body as opposed to in the URL as with GET request.
-In our example, if you want to deploy a deep learning model it will be easier and more secure to send parameter via a POST body
-We'll specify a route, json-example, just to distinguish it from the previous example
+In our example, if you want to deploy a deep learning model it will be easier to send parameter via a POST body
+We'll specify a new route, json-example, just to distinguish it from the previous example
 We can get the information in the body by calling request.get_json() or request.json
-Then we create a response message and return it back
+Then we will formulate a response message and return it back
 '''
 @application.route('/json-example', methods=['POST'])
 def jsonexmaple():
@@ -50,7 +50,7 @@ def jsonexmaple():
 '''
 Example 3: Model Inference
 In this example we'll use a keras model for inference
-Load  your model and any other preprocessing dependencies(tokenizer) in the global scope so that it is cache in memory
+Load your model and any other preprocessing dependencies(tokenizer) in the global scope so that it is cache in memory
 '''
 with open('./dumps/tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
@@ -81,8 +81,8 @@ def keras_exmample():
 
 
 # run it via
-# curl -X POST http://127.0.0.1:5000/keras-example -d '{"message":"this is a message"}' -H "Content-Type: application/json"
+# curl -X POST http://127.0.0.1:5000/keras-example -d '{"message":"I am extremely happy!"}' -H "Content-Type: application/json"
 # it'll return model inference probability back
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(host="0.0.0.0", port=5000, debug=True)
